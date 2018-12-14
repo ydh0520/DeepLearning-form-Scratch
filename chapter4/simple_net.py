5d8a703eda2c91e38f93e86e6563a2d7506b2b07
ydh0520 <ydh2338@naver.com>
2018-12-14 10:24:42 +0900

    chapter4

--- chapter4/simple_net.py
+++ chapter4/simple_net.py
@@ -17,3 +17,22 @@ class simpleNet:
         loss=cross_entropy_error(y,t)
 
         return loss
+
+def f(W):
+    return net.loss(x,t)
+
+net =simpleNet()
+print(net.W)
+
+x=np.array([0.6,0.9])
+p=net.predict(x)
+
+print(p)
+
+np.argmax(p)
+
+t=np.array([0,0,1])
+net.loss(x,t)
+
+dW=numerical_gradient(f,net.W)
+print(dW)
