b9e60590b10842e50d31521639ca4413d275e58e
ydh0520 <ydh2338@naver.com>
2018-12-17 10:50:13 +0900

    two_layer_net

--- chapter4/two_layer_net.py
+++ chapter4/two_layer_net.py
@@ -9,8 +9,8 @@ class TwoLayerNet:
         self.params={}
         self.params['W1']=weight_init_std*np.random.randn(input_size,hidden_size)
         self.params['b1']=np.zeros(hidden_size)
-        self.params['W2']=weight_init_std*np.random.randn(input_size,hidden_size)
-        self.params['b2']=np.zeros(hidden_size)
+        self.params['W2']=weight_init_std*np.random.randn(hidden_size,output_size)
+        self.params['b2']=np.zeros(output_size)
         
     def predict(self,x):
         W1,W2=self.params['W1'],self.params['W2']
@@ -18,7 +18,7 @@ class TwoLayerNet:
 
         a1=np.dot(x,W1)+b1
         z1=sigmoid(a1)
-        a2=np.dot(a1,W2)+b2
+        a2=np.dot(z1,W2)+b2
         y=softmax(a2)
 
         return y
@@ -46,3 +46,20 @@ class TwoLayerNet:
         grads['b2']=numerical_gradient(loss_W,self.params['b2'])
 
         return grads
+
+net = TwoLayerNet(input_size=784,hidden_size=100,output_size=10)
+
+print(net.params['W1'].shape)
+print(net.params['b1'].shape)
+print(net.params['W2'].shape)
+print(net.params['b2'].shape)
+
+x=np.random.rand(100,784)
+t=np.random.rand(100,10)
+
+grads=net.numerical_gradient(x,t)
+
+print(grads['W1'].shape)
+print(grads['b1'].shape)
+print(grads['W2'].shape)
+print(grads['b2'].shape)
