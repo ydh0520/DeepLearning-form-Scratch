0e86ca799b6b01bccd536d013ce5e896f926cd27
ydh0520 <ydh2338@naver.com>
2018-12-18 16:56:21 +0900

    test but_apple_orange

--- chapter5/buy_apple_orange.py
+++ chapter5/buy_apple_orange.py
@@ -17,6 +17,16 @@ add_apple_orange_layer=AddLayer()
 apple_price=mul_apple_layer.forward(apple,apple_num)
 orange_price=mul_orange_layer.forward(orange,orange_num)
 total_price=add_apple_orange_layer.forward(apple_price,orange_price)
-price=mul_tax_layer.forward(tax,total_price)
+price=mul_tax_layer.forward(total_price,tax)
 
 print(price)
+
+dprice=1
+dtotal_price,dtax=mul_tax_layer.backward(dprice)
+print(dtotal_price,dtax)
+dapple_price,dorange_price=add_apple_orange_layer.backward(dtotal_price)
+print(dapple_price,dorange_price)
+dapple,dapple_num=mul_apple_layer.backward(dapple_price)
+print(dapple,dapple_num)
+dorange,dorange_num=mul_orange_layer.backward(dorange_price)
+print(dorange,dorange_num)
