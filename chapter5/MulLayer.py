27297cb3d9c7acd940a5ad54e0f2bcd0f95b56d4
ydh0520 <ydh2338@naver.com>
2018-12-18 16:33:24 +0900

    MulLayer test

--- chapter5/MulLayer.py
+++ chapter5/MulLayer.py
@@ -15,3 +15,23 @@ class MulLayer:
         dy=dout*self.x
 
         return dx,dy
+
+#순전파
+apple=100
+apple_num=2
+tax=1.1
+
+mul_apple_layer = MulLayer()
+mul_tax_layer = MulLayer()
+
+apple_price = mul_apple_layer.forward(apple,apple_num)
+price=mul_tax_layer.forward(apple_price,tax)
+
+print(price)
+
+#역전파
+dprice=1
+dapple_price,dtax=mul_tax_layer.backward(dprice)
+dapple,dappel_num=mul_apple_layer.backward(dapple_price)
+
+print(dapple, dappel_num, dtax)
