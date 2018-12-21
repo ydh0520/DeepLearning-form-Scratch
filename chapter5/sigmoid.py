c2d29ea235d246af64face47ab8f6e4c848ea026
ydh0520 <ydh2338@naver.com>
2018-12-21 14:01:33 +0900

    sigmoid error fix

--- chapter5/sigmoid.py
+++ chapter5/sigmoid.py
@@ -5,13 +5,12 @@ class Sigmoid:
         self.out=None
 
     def forward(self,x):
-        out=1/(1+np.epx(-x))
+        out=1/(1+np.exp(-x))
         self.out=out
 
         return out
 
-    def backward(self,dout)
+    def backward(self,dout):
         dx=dout*(1.0-self.out)*self.out
 
         return dx
-
